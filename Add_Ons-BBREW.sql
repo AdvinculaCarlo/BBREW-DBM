912b7d5b90905cc342b642a68793286990a0aafb
Carlo Advincula <takinocaloy16@gmail.com>
2024-09-27 00:59:03 +0800

    1
    Updated

---
+++ Add_Ons-BBREW.sql
@@ -0,0 +1,16 @@
+/* CREATE TABLE Add_Ons (
+Add_OnsID INT(5) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+Add_Ons_Name VARCHAR(100) NOT NULL UNIQUE,
+Add_Ons_Price INT(50) NOT NULL UNIQUE
+); */
+-- Insert data into the table --
+INSERT INTO Add_Ons (Add_Ons_Name, Add_Ons_Price) 
+VALUES
+('Pearl', 9.00),
+('Crystal', 9.00),
+('Cream Cheese', 9.00),
+('Cream Puff', 9.00),
+('Cheesecake', 9.00),
+('Crushed Oreo', 9.00),
+('Coffee Jelly', 9.00),
+('Whipped Cream', 9.00);
